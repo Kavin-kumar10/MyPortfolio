cad9ede9c470ca45cc213940b61dd13ea7025257
kavin-kumar10 <kavinkumar.prof@gmail.com>
2023-01-01 16:58:25 +0530

    Preloader and Scrollbar edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {React,useState,useEffect} from 'react';
 import './App.scss';
 import Landpage from './Components/Landpage';
 import Nav from './Components/Nav';
@@ -5,16 +6,30 @@ import About from './Components/About';
 import Skills from './Components/Skills';
 import Project from './Components/Project';
 import TimeLine from './Components/TimeLine';
+import Preloader from './Components/Preloader';
 
 function App() {
+  const [isLoading,setLoading] = useState(true);
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false);
+    },1000);
+  },[])
   return (
-    <div className="App">
-      <Nav/>
-      <Landpage/>
-      <About/>
-      <Skills/>
-      <Project/>
-      <TimeLine/>
+    <div>
+      {
+        isLoading?(<Preloader/>)
+        :
+        (<div className='App'>
+          <Nav/>
+          <Landpage/>
+          <About/>
+          <Skills/>
+          <Project/>
+          <TimeLine/>
+        </div>)
+      }
     </div>
   );
 }
