98822a7f9c7a5b519f7051c65330d271bb87a645
kavin-kumar10 <kavinkumar.prof@gmail.com>
2023-02-25 21:02:43 +0530

    Contact page added with emailjs

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BiSend} from 'react-icons/bi'
 import {AiFillLinkedin,AiFillGithub,AiOutlineWhatsApp} from 'react-icons/ai'
 import {RxReset} from 'react-icons/rx'
 import {SiLeetcode} from 'react-icons/si'
 import {ReactComponent as Contact_img} from '../../Assets/Contact.svg';
+import emailjs from 'emailjs-com'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./Contact.scss"
 
 const Contact = () =>{
+    const [username,setUserName] = useState('');
+    const [mail,setMail] = useState('');
+    const [desc,setDesc] = useState('');
+    function sendEmail(e){
+        e.preventDefault();
+        emailjs.sendForm('service_2yvf8lr','template_dhy4q7w',e.target,'sB99l46nzAZ-CCuM0')
+        .then((result)=>{
+            console.log(result);
+            notify();
+            setMail('');
+            setUserName('');
+            setDesc('');
+        })
+        .catch(err=>console.log(err))
+    }
+    function reset(){
+        setMail('');
+        setUserName('');
+        setDesc('');
+    }
+    const notify = () => toast("Mail Sent Successfully thank you!");
     return(
         <div className="Contact" id='Contact'>
+            <ToastContainer
+            position="top-left"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+            />
             <div className="Details">
                 <div className="Info">
                     <h1>Feel Free <span>2</span> Contact</h1>
@@ -39,22 +75,22 @@ const Contact = () =>{
                 <div className="Icon">
                     <Contact_img id="Contact_img"/>
                 </div>
-                <form action="mailto:kavinkumar.prof@gmail.com">
+                <form onSubmit={sendEmail}>
                     <div className="input">
                         <h2>Username</h2>
-                        <input type="text" placeholder='Kavin Kumar M'/>
+                        <input name='name' value={username} type="text" placeholder='Kavin Kumar M' onChange={(e)=>{setUserName(e.target.value)}} required/>
                     </div>
                     <div className="input">
                         <h2>Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
-                        <input type="email" placeholder='kavinkumar****@gmail.com'/>               
+                        <input name='User-email' value={mail} type="email" placeholder='kavinkumar****@gmail.com' onChange={(e)=>{setMail(e.target.value)}} required/>               
                     </div>                 
                     <div className="input">
                         <h2>Desc &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
-                        <textarea id="textarea" placeholder='Description' cols="30" rows="10"/>
+                        <textarea name='message' id="textarea" value={desc} placeholder='Description' cols="30" rows="10" onChange={(e)=>{setDesc(e.target.value)}} required/>
                     </div>  
                     <div className="button">
-                        <button id='submit' type='submit' value="send" >Send <BiSend id='btn' size={25}/></button>
-                        <button id='reset' type = "reset">Reset <RxReset id='btn' size={25}/></button>
+                        <button id='submit' type='submit'>Send <BiSend id='btn' size={25}/></button>
+                        <button id='reset' type = "reset" onClick={reset}>Reset <RxReset id='btn' size={25}/></button>
                     </div>
                 </form>
             </div>
